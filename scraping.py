bef729a7634674ad327511957afd1d078ea48908
twugo <hm17031@student.miyazaki-u.ac.jp>
2020-01-07 16:23:32 +0900

    改ページ処理追加

--- scraping.py
+++ scraping.py
@@ -8,14 +8,18 @@ import codecs
 
 filename = "alice.txt"
 
-h1StartMkup = "[size=30][b]"
-h1EndMkup = "[/b][/size]"
-h2StartMkup = "[size=25][b]"
-h2EndMkup = "[/b][/size]"
+h1StartMkup = "[size=30][b][i][color=#dd2fc0]"
+h1EndMkup = "[/color][/b][/i][/size]"
+h2StartMkup = "[size=25][b][i][color=#c0dd2f]"
+h2EndMkup = "[/color][/b][/i][/size]"
 preStartMkup = "[i]"
 preEndMkup = "[/i]"
 
-target_url = 'https://www.gutenberg.org/files/11/11-h/11-h.htm' # 不思議の国のアリス
+
+target_url = 'https://www.gutenberg.org/files/11/11-h/11-h.htm' # 不思議な国のアリス
+#target_url = 'https://www.gutenberg.org/files/1513/1513-h/1513-h.htm' # ロミオとジュリエット
+#target_url = 'https://www.gutenberg.org/files/1661/1661-h/1661-h.htm' # シャーロックホームズ
+
 
 result = requests.get(target_url)
 result.raise_for_status() # 例外処理
@@ -34,10 +38,12 @@ for tag in soup.find_all("h2"):
     tag.insert(0, h2StartMkup)
     tag.append(h2EndMkup)
 
-for tag in soup.find_all(["pre"]):
-    tag.insert(0, preStartMkup)
-    tag.append(preEndMkup)
+for tag in soup.find_all("pre"):
+    #tag.insert(0, preStartMkup)
+    #tag.append(preEndMkup)
+    tag.string = preStartMkup + tag.getText() + preEndMkup
 
+# ファイル書き込み
 textStartFlag = 0
 with open(filename, "w", encoding="UTF-8") as f:
     for e in soup.find_all(["p", "h1", "h2", "pre", "td"]):
@@ -48,3 +54,23 @@ with open(filename, "w", encoding="UTF-8") as f:
             if h1StartMkup in tmp:
                 textStartFlag = 1
                 print(tmp, file=f)
+
+# 改行処理
+with open(filename, encoding="UTF-8") as f:
+    column = 0
+    pagecolumn = 20
+
+    text = f.readlines()
+    while True:
+        if column == len(text):
+            break
+        
+        if h2StartMkup in text[column]:
+            while column % pagecolumn != 0:
+                text.insert(column, "\n")
+                column += 1
+        column += 1
+
+# 改行したデータでファイルを上書き
+with open(filename, "w", encoding="UTF-8") as f:
+    f.writelines(text)
